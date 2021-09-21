1093b1cae206f701d32abd46c9f187159b471ee3
hutao <hutaoandroid@163.com>
2021-09-21 20:52:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myjetpack/viewmodeldemo/NumActivity.java
@@ -0,0 +1,7 @@
+package com.example.myjetpack.viewmodeldemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NumActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/myjetpack/viewmodeldemo/NumVIewModel.java
@@ -0,0 +1,7 @@
+package com.example.myjetpack.viewmodeldemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class NumVIewModel extends ViewModel {
+    public int number = 0;
+}
